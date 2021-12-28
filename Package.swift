de36085a1d9b29d0002c33613293e642055cf5e9
Marko Hlebar <marko.hlebar@gmail.com>
2021-12-28 10:23:27 +0100

    Fix for target name

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SonectSDK",
-            targets: ["SonectSDK"]),
+            targets: ["Sonect"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "SonectSDK",
+            name: "Sonect",
             url: "https://github.com/sonect/sonect-sdk-ios/releases/download/4.8.3/SonectSDK_Cocoapods.framework.zip",
             checksum: "da8e0149fb2c556a83c0feca77e48c1ee1eb0a44b31928c0fe5d6869f2f56d8a"
         )
